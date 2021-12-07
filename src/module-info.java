8228e1d948547e3b9a4823f61899af3d86703e56
brxoo <brunoaraujo198@gmail.com>
2021-12-07 14:38:42 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -4,4 +4,5 @@ module workshop_javafx_jdbc {
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 }
